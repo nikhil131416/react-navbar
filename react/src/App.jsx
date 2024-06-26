e8f56bb5990dcacb2e278d92f0dcfca1dfc44183
nikhil <nikhildodtalle1314@gmail.com>
2024-06-26 22:35:39 +0530

    first commit

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
 import Nav from './Nav'
+import Hero from './Hero'
 function App() {
 
   return (
     <>
     <Nav/>
-      
+      <Hero/>
     </>
   )
 }
---
+++ react/src/Hero.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Hero.css'
+
+export default function Hero() {
+  return (
+    <div className='hero'>
+ <div className="left">
+<div className="para">
+    <p>    Web Developer    </p>
+</div>
+<div className="herosectionh1">
+    <h1>I'm <span>Nikhil</span></h1>
+</div>
+<div className="stack">
+    <h1> Full Stack Developer</h1>
+</div>
+<div className="para2">
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum omnis nihil possimus nam deserunt repellendus similique, est sapiente, dolorem vel molestiae reiciendis illo nesciunt explicabo tempora! Quia, tempora suscipit.
+
+    </p>
+</div>
+ </div>
+ <div className="right">
+ </div>
+      
+    </div>
+  )
+}
